2a8a3b5eb229b4e9ace11a8c5d6378e6b0ac8da2
Daniel Ng <ngenator@gmail.com>
2018-01-06 21:09:41 -0500

    Tweaks

--- robot/scripts/setup.sh
+++ robot/scripts/setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-export ROBOT_ADDRESS="${ROBOT_ADDRESS:-10.42.0.138}"
+ROBOT_ADDRESS="${ROBOT_ADDRESS:-10.42.0.138}"
+
+cat ~/.ssh/id_rsa.pub | ssh ngenator@$ROBOT_ADDRESS 'cat >> .ssh/authorized_keys'
