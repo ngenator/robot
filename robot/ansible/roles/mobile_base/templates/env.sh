424507354cbc94bc5c39075b6994138b7ffb4be5
Daniel Ng <ngenator@users.noreply.github.com>
2019-10-04 16:02:37 -0400

    Fix some broken things

--- robot/ansible/roles/mobile_base/templates/env.sh
+++ robot/ansible/roles/mobile_base/templates/env.sh
@@ -6,4 +6,4 @@ source /home/{{ ros_user.name }}/catkin_ws/devel/setup.bash
 export ROS_IP="{{ ansible_default_ipv4.address  }}"
 export ROS_MASTER_URI="http://{{ ansible_default_ipv4.address }}:11311"
 
-exec @
+exec $@
