baed76708869be79ac5f5236c2713c6cac7c5f44
Daniel Ng <ngenator@users.noreply.github.com>
2019-10-04 10:55:31 -0400

    Updating ansible

---
+++ robot/ansible/README.md
@@ -0,0 +1,6 @@
+# TODO
+
+* create ad-hoc command to install and configure sshd on each host
+  * install openssh-server
+  * generate ssh keys
+* create ad-hoc command to push public keys to each host
---
+++ robot/ansible/roles/mobile_base/templates/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source /opt/ros/{{ ros_distribution }}/setup.bash
+source /home/{{ ros_user.name }}/catkin_ws/devel/setup.bash
+
+export ROS_IP="{{ ansible_default_ipv4.address  }}"
+export ROS_MASTER_URI="http://{{ ansible_default_ipv4.address }}:11311"
+
+exec @
